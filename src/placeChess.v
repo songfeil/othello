e0a647fd86653267a4e25e1e49e0357e89495058
Chaohan Zhu <mikelg.zhu@mail.utoronto.ca>
2018-03-25 16:50:20 -0400

    New states and HEX

--- src/placeChess.v
+++ src/placeChess.v
@@ -2,13 +2,14 @@
 module control(
     input clk,
     input restart,
-    input go,
+    input go, jump,
+	 input confirm,
 	 input move_up, move_down, move_left, move_right, place,
 	 input win,
 	 
 //    output reg ld_key,
 //	 output reg [3:0] select_ld
-	 output reg turn_side,
+	 output reg turn_side, detect,
     output reg plot_empty, draw_cell, place_disk,
 	 output [3:0] state,
 	 output [3:0] ns
@@ -19,20 +20,27 @@ module control(
 	 assign state[3:0] = current_state[3:0];
 	 assign ns[3:0] = next_state[3:0];
 	 
-	 assign en = 0;
-	 //assign en = move_up || move_down || move_left || move_right;
+//	 assign en = 0;
+	 assign en = move_up | move_down | move_left | move_right;
     localparam  START_GAME   = 4'd0,
                 DRAW_BOARD   = 4'd1,
-                B_SELECT     = 4'd2,
-                B_PLACE  	  = 4'd3,
-                W_SELECT     = 4'd4,
-                W_PLACE      = 4'd5,
-                END_GAME     = 4'd6,
-                S_CYCLE_1    = 4'd7,
-                S_CYCLE_2    = 4'd8,
-					 S_CYCLE_3    = 4'd9,
-					 S_CYCLE_4    = 4'd10;
-    
+					 
+					 B_WAIT       = 4'd2,
+                B_SELECT     = 4'd3,
+					 S_CYCLE_1    = 4'd4,
+                S_CYCLE_2    = 4'd5,
+					 B_DETECT     = 4'd6,
+                B_PLACE  	  = 4'd7,
+					 
+					 W_WAIT		  = 4'd8,
+                W_SELECT     = 4'd9,
+					 S_CYCLE_3    = 4'd10,
+					 S_CYCLE_4    = 4'd11,
+					 W_DETECT     = 4'd12,
+                W_PLACE      = 4'd13,
+                
+					 END_GAME     = 4'd14;
+                
     // Next state logic aka our state table
     always@(*)
     begin: state_table 
@@ -40,26 +48,34 @@ module control(
                 START_GAME: next_state = go ? DRAW_BOARD : START_GAME; // Loop in current state until value is input
                 DRAW_BOARD: next_state = B_SELECT; // Loop in current state until go signal goes low
                 
+					 B_WAIT: next_state = jump ? B_WAIT : W_SELECT;
 					 B_SELECT: begin
-						 if (place)
-								next_state = B_PLACE;
+						 if (jump)
+								next_state = B_WAIT;
+						 else if (place)
+								next_state = B_DETECT;
 						 else
 								next_state = en ? S_CYCLE_1 : B_SELECT;
 						 end // 
                 S_CYCLE_1: next_state = S_CYCLE_2; // Loop in current state until go signal goes low
                 S_CYCLE_2: next_state = B_SELECT; // Loop in current state until value is input
+					 B_DETECT:  next_state = confirm ? B_PLACE : B_SELECT;
                 B_PLACE: next_state = win ? END_GAME : W_SELECT; // Loop in current state until go signal goes low
                 
 					 END_GAME: next_state = en ? START_GAME : END_GAME;
 					 
+					 W_WAIT: next_state = jump ? W_WAIT : B_SELECT;
 					 W_SELECT: begin
-						 if (place)
-								next_state = W_PLACE;
+						 if (jump)
+								next_state = B_SELECT;
+						 else if (place)
+								next_state = W_DETECT;
 						 else
 								next_state = en ? S_CYCLE_3 : W_SELECT;
 						 end
 					 S_CYCLE_3: next_state = S_CYCLE_4;
 					 S_CYCLE_4: next_state = W_SELECT; // we will be done our two operations, start over after
+					 W_DETECT: next_state = confirm ? W_PLACE : W_SELECT;
 					 W_PLACE: next_state = win ? END_GAME : B_SELECT;
 					 
             default:     next_state = START_GAME;
@@ -110,6 +126,10 @@ module control(
 //					 select_ld = 4'd2 // load selected cell pic
                 draw_cell = 1'b1;
 					 end
+				B_DETECT: begin
+					 detect = 1'b1;
+					 end
+					 
 				B_PLACE: begin
 //					 select_ld = 4'd14; // load black in cell
 					 place_disk = 1'b1;
@@ -133,6 +153,8 @@ module control(
 //					 select_ld = 4'd2 // load selected cell pic
                 draw_cell = 1'b1;
 					 end
+				W_DETECT: begin
+					 detect = 1'b1;
 				W_PLACE: begin
 //					 select_ld = 4'd15; // load white in cell
 					 place_disk = 1'b1;
